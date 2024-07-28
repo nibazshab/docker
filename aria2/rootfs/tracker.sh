cd95f76406fdb1d2bfa662d927b08b6df32800aa
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-07-28 15:53:28 +0000

    less

--- aria2/rootfs/tracker.sh
+++ aria2/rootfs/tracker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-url=http://localhost:6800/jsonrpc
 tracker=$(curl -sL https://trackerslist.com/all_aria2.txt)
+
 [ -n $tracker ] && {
-  curl -d '{"id":"1","method":"aria2.changeGlobalOption","params":["token:'$key'",{"bt-tracker":"'$tracker'"}]}' $url
+  curl -d '{"id":"1","method":"aria2.changeGlobalOption","params":["token:'$key'",{"bt-tracker":"'$tracker'"}]}' http://localhost:6800/jsonrpc
   sed -i "s@^\(bt-tracker=\).*@\1$tracker@" /c/aria2.conf
 }
